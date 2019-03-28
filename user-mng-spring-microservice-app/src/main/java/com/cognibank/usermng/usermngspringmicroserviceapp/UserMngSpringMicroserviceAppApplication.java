715ab73e6fa201275fa0045586da4ca03cddbc44
alokbg11 <alokbg11@gmail.com>
2019-03-28 14:58:12 -0400

    initial setup

---
+++ user-mng-spring-microservice-app/src/main/java/com/cognibank/usermng/usermngspringmicroserviceapp/UserMngSpringMicroserviceAppApplication.java
@@ -0,0 +1,13 @@
+package com.cognibank.usermng.usermngspringmicroserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMngSpringMicroserviceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMngSpringMicroserviceAppApplication.class, args);
+	}
+
+}
