6e6d5268c126999f77b7a9c532c4e2e0936f5b51
seshu <spcgkd@mail.umkc.edu>
2019-04-11 15:50:21 -0400

    updated the swagger config

--- src/main/java/com/cognibank/usermng/usermngspringmicroserviceapp/config/SwaggerConfig.java
+++ src/main/java/com/cognibank/usermng/usermngspringmicroserviceapp/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -22,7 +23,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.cognibank.usermng.usermngspringmicroserviceapp.controller"))
-                .paths(regex("users/management/.*"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(metaData());
     }
